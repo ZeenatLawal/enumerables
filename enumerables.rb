1f789aa7c4ec5028f47b185eab122a7f1ea3462c
ZeenatLawal <zeenatlawal82@gmail.com>
2021-04-21 15:57:39 +0100

    create my_select method

--- enumerables.rb
+++ enumerables.rb
@@ -13,5 +13,26 @@ module Enumerable
       b += 1
     end
   end
+
+  def my_select
+   array = []
+   self.my_each do |x|
+     if yield x
+      array.push(x)
+    end
+    array
+   end
+  end
 end
 # rubocop:enable Style/For
+# friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+# invited_list = []
+
+# friends.each do |friend|
+#   if friend != 'Brian'
+#     invited_list.push(friend)
+#   end
+# end
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.my_select { |friend| friend != 'Brian' }
