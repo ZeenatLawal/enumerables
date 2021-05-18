9c99bd06894b35afece0046563ae2765fc6e579e
Lucky <vagyasri2011@gmail.com>
2021-05-18 21:40:27 +0200

    Change example

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require './enumerables'
 
 RSpec.describe Enumerable do
-  array = [1, 2, 3]
+  array = %w[Sharon Leo Leila Brian Arun]
   describe '#my_each' do
     context 'if block not given' do
       it 'returns enum' do
@@ -13,8 +13,8 @@ RSpec.describe Enumerable do
     context 'if block given' do
       it 'yields index' do
         arr = []
-        array.my_each { |index| arr.push(index**3) }
-        expect(arr).to eq([1, 8, 27])
+        array.my_each { |index| arr.push(index) }
+        expect(arr).to eq(%w[Sharon Leo Leila Brian Arun])
       end
     end
   end
