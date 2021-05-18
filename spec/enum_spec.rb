793c06f4954b3c0cd10d85bba0603a1e2e4f6298
Lucky <vagyasri2011@gmail.com>
2021-05-18 22:12:01 +0200

    Describe my_each_with_index method

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -11,9 +11,25 @@ RSpec.describe Enumerable do
     end
 
     context 'if block given' do
-      it 'yields index' do
-        array.my_each { |index| puts index }
+      it 'yields item' do
+        array.my_each { |friend| puts friend }
         expect(array).to eq(%w[Sharon Leo Leila Brian Arun])
+      end
+    end
+  end
+
+  describe '#my_each_with_index' do
+    context 'if block not given' do
+      it 'returns enum' do
+        expect(array.my_each_with_index).to be_an Enumerator
+      end
+    end
+
+    context 'if block given' do
+      it 'yields item with index' do
+        arr = []
+        array.my_each_with_index { |friend, index| arr.push("#{index}: #{friend}") if index.odd? }
+        expect(arr).to eq(["1: Leo", "3: Brian"])
       end
     end
   end
